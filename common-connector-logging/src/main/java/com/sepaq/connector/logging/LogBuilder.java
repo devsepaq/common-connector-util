97ffd31887cde9b01d0d34d477df042aa0d4124d
Mathieu Jalbert <jalbert.mathieu@sepaq.com>
2021-03-04 09:40:20 -0500

    New version to set fields correctly

--- common-connector-logging/src/main/java/com/sepaq/connector/logging/LogBuilder.java
+++ common-connector-logging/src/main/java/com/sepaq/connector/logging/LogBuilder.java
@@ -22,12 +22,15 @@ public class LogBuilder {
     TRACE, DEBUG, INFO, ERROR, WARN
   }
 
-
-
   private Map<String, Object> fields = new LinkedHashMap<String, Object>();
   private Level level = Level.INFO;
   private Logger logger;
   private Throwable throwable;
+  
+  private String activity;
+  private String appCode;
+  private String correlationId;
+  private String payload;
 
   private LogBuilder(Class<?> clazz) {
     this.logger = LoggerFactory.getLogger(clazz);
@@ -44,8 +47,6 @@ public class LogBuilder {
   public static LogBuilder newInstance(String name) {
     return new LogBuilder(name);
   }
-
-
 
   public String sanitize(Object payload) {
     return Objects.toString(payload)//
@@ -76,43 +77,46 @@ public class LogBuilder {
   @Override
   public String toString() {
     StringBuilder sb = new StringBuilder();
+    sb.append(String.format("%s=\"%s\", ", APP_CODE, appCode));
+    sb.append(String.format("%s=\"%s\", ", ACTIVITE, activity));
+    sb.append(String.format("%s=\"%s\", ", CORRELATION_ID, correlationId));
     for (Entry<String, Object> field : fields.entrySet()) {
       if (field.getValue() != null) {
         sb.append(String.format("%s=\"%s\", ", field.getKey(), field.getValue()));
       }
     }
+    sb.append(String.format("%s=\"%s\", ", PAYLOAD, payload));
 
     return sb.toString();
   }
 
   public LogBuilder withAppCode(String appCode) {
-    fields.put(APP_CODE, appCode);
+    this.appCode = appCode;
     return this;
   }
 
   public LogBuilder withPayload(Object payload) {
-    fields.put(PAYLOAD, sanitize(payload));
+    this.payload = sanitize(payload);
     return this;
   }
 
   public LogBuilder withActivity(String activity) {
-    fields.put(ACTIVITE, activity);
+    this.activity = activity;
     return this;
   }
 
   public LogBuilder withCorrelationId(String correlationId) {
-    fields.put(CORRELATION_ID, correlationId);
+    this.correlationId = correlationId;
     return this;
   }
-
 
   public LogBuilder withSeverity(Level level) {
     this.level = level;
     return this;
   }
 
-  public LogBuilder withOtherFields(String key, Object value) {
-    fields.put(key, value);
+  public LogBuilder withOtherFields(Map<String, Object> otherFields) {
+    this.fields = otherFields;
     return this;
   }
 
@@ -120,6 +124,5 @@ public class LogBuilder {
     this.throwable = exception;
     return this;
   }
-
 
 }
